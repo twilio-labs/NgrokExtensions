e2fb3ca8fb959dde69ff8399808ac74d3a1d3ecc
Tim Heuer <tim@timheuer.com>
2022-01-11 15:58:00 -0800

    Initial support for VS2022 #47

--- src/NgrokExtensionsSolution/NgrokExtensions/NgrokUtils.cs
+++ src/NgrokExtensionsSolution/NgrokExtensions/NgrokUtils.cs
@@ -12,13 +12,14 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Net.Http.Json;
 
 namespace NgrokExtensions
 {
     public class NgrokUtils
     {
         public const string NgrokNotFoundMessage = "ngrok executable not found. Configure the path in the via the add-in options or add the location to your PATH.";
-        private static Version MinimumVersion = new Version("2.3.34");
+        private static readonly Version MinimumVersion = new Version("2.3.34");
         private readonly Dictionary<string, WebAppConfig> _webApps;
         private readonly Func<string, Task> _showErrorFunc;
         private readonly HttpClient _ngrokApi;
