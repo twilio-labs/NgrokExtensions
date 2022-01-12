6d762781623d6a674be8869cda35d57cb71a9db5
Tim Heuer <tim@timheuer.com>
2022-01-12 09:36:22 -0800

    Initial stab at splitting out shared code to work with VS2019/2022

---
+++ src/NgrokExtensionsSolution/NgrokExtensions.Shared/OptionsPageGrid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NgrokExtensions
+{
+    public class OptionsPageGrid : Microsoft.VisualStudio.Shell.DialogPage
+    {
+        private string executablePath = "";
+
+        [Category("ngrok")]
+        [DisplayName("Executable Path")]
+        [Description("Full path to the ngrok executable")]
+        public string ExecutablePath
+        {
+            get { return executablePath; }
+            set { executablePath = value; }
+        }
+    }
+}
---
+++ src/NgrokExtensionsSolution/NgrokExtensions.Shared/StartTunnelPackage.cs
@@ -0,0 +1,75 @@
+﻿// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+// Copyright (c) 2019 David Prothero
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace NgrokExtensions
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(StartTunnelPackage.PackageGuidString)]
+    [ProvideOptionPage(typeof(OptionsPageGrid), "ngrok", "Options", 0, 0, true)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class StartTunnelPackage : Package
+    {
+        /// <summary>
+        /// StartTunnelPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "9f845cfc-84ef-4aac-9826-d46a83744fb4";
+
+        public string ExecutablePath
+        {
+            get
+            {
+                OptionsPageGrid page = (OptionsPageGrid)this.GetDialogPage(typeof(OptionsPageGrid));
+                return page.ExecutablePath;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StartTunnel"/> class.
+        /// </summary>
+        public StartTunnelPackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            StartTunnel.Initialize(this);
+            base.Initialize();
+        }
+
+        #endregion
+    }
+}
